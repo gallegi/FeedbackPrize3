b293a162609dd77fb61e9b54ab069e48f7807983
Nguyen The Nam <namnguyen61031@gmail.com>
2022-11-02 09:28:02 +0700

    Not reinit embedding layers

--- src/model.py
+++ src/model.py
@@ -79,10 +79,10 @@ class LitFB3(pl.LightningModule):
             module.weight.data.normal_(mean=0.0, std=self.model_config.initializer_range)
             if module.bias is not None:
                 module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.model_config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
+        # elif isinstance(module, nn.Embedding):
+        #     module.weight.data.normal_(mean=0.0, std=self.model_config.initializer_range)
+        #     if module.padding_idx is not None:
+        #         module.weight.data[module.padding_idx].zero_()
         elif isinstance(module, nn.LayerNorm):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
